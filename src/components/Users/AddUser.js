9ff1b1355fecddc415c2dac01d047acf98c7bb0f
Pavithran <pavithrank02@gmail.com>
2022-09-26 20:00:49 +0530

    Wrapper used

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,7 +3,7 @@ import Card from '../UI/Card'
 import Button from '../UI/Button';
 import ErrorModal from "../UI/ErrorModal";
 import classes from './AddUser.module.css';
-
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = props => {
 
@@ -42,7 +42,7 @@ const errorHandler = () => {
     setError(null);
 }
     return (
-			    <div>
+			    <Wrapper>
                   {  error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}					
                     <Card className={classes.input}>
 							<form onSubmit={addUserHandler}>
@@ -53,7 +53,7 @@ const errorHandler = () => {
 									<Button type="submit">Add User</Button>
 							</form>
 					</Card>
-				</div>
+				</Wrapper>
         
         
     )
